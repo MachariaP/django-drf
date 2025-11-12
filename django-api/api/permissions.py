6d8f0bcced5c715eba82d311a2f9ef298d449602
Phinehas <walburphinehas78@gmail.com>
2025-11-12 11:55:04 +0300

    Feat: Add custom permission classes for access control

---
+++ django-api/api/permissions.py
@@ -0,0 +1,36 @@
+from rest_framework import permissions
+
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow authors of an object to edit it.
+    """
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        # Write permissions are only allowed to the author
+        return obj.author == request.user
+
+
+class IsReviewOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow review owners to edit their reviews.
+    """
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        return obj.user == request.user
+
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow admins to edit.
+    """
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        return request.user and request.user.is_staff
